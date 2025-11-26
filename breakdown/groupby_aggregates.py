c127b199b67a3ed7116727e078584aff0d7b3a9b
Sunny242545 <chakrisundeep007@gmail.com>
2025-11-25 23:41:09 -0500

    working on group by and order by

--- breakdown/groupby_aggregates.py
+++ breakdown/groupby_aggregates.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 
 #Creating a Spark Session
 print("Creating Spark Session....")
@@ -8,6 +9,15 @@ spark: SparkSession =(
     .getOrCreate()
 )
 
+#Reading the CSV file into a Dataframe
+print("Reading a CSV file into Dataframe:")
+df = spark.read.csv('employe_with_department.csv', header = True, inferSchema = True)
+print("This is the original data of a dataframe we got from CSV")
+df.show()
+
+#Check the total salary for each department
+filtered_df = df.groupBy('Department').agg(F.sum('Salary').alias('TotalSalary')).orderBy('TotalSalary', ascending = False)
+filtered_df.show()
 
 
 
